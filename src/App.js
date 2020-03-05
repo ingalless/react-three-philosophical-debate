9a60f49feb2703fae1eb532d1c0b2810e68854ac
Jonathan Ingall <jonny@mondago.com>
2020-03-05 12:18:39 +0000

    Add momentum to cube

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import Box from "./Box";
 
 function App() {
   return (
-    <Canvas style={{ height: "100vh", width: "100vw" }}>
-      <ambientLight />
-      <pointLight position={[16, 7, 5]} />
-      <perspectiveCamera fov={75} aspect={2} near={0.1} far={5} />
-      <Suspense fallback="loading..."><Box position={[0, 0, 0]} /></Suspense>
-    </Canvas>
+    <>
+      <h1 style={{ textAlign: "center" }}>CultureGo is not your friend</h1>
+      <Canvas style={{ height: "90vh", width: "100vw" }}>
+        <ambientLight />
+        <pointLight position={[16, 7, 5]} />
+        <perspectiveCamera fov={75} aspect={2} near={0.1} far={5} />
+        <Suspense fallback="loading...">
+          <Box position={[0, 0, 0]} />
+        </Suspense>
+      </Canvas>
+    </>
   );
 }
 
--- src/Box.js
+++ src/Box.js
@@ -1,41 +1,62 @@
-import React, { useRef, useMemo, useEffect, useLayoutEffect } from "react";
+import React, {
+  useRef,
+  useMemo,
+  useEffect,
+  useLayoutEffect,
+  useState
+} from "react";
 import { useFrame, useLoader } from "react-three-fiber";
-import { TextureLoader, MeshBasicMaterial } from "three";
+import { TextureLoader, MeshBasicMaterial, MathUtils } from "three";
 import box from "./box.jpg";
 
 export default function Box(props) {
   const mesh = useRef();
   const texture = useLoader(TextureLoader, box);
+  const [state, setState] = useState({ x: 0, y: 0, isMoving: false });
+
+  useFrame(() => {
+    const currentState = state;
+    if (state.isMoving) {
+      mesh.current.rotation.x += currentState.y /= 1.1;
+      mesh.current.rotation.y += currentState.x /= 1.1;
+
+      if (Math.abs(currentState.x) + Math.abs(currentState.y) < 0) {
+        setState({ ...currentState, isMoving: false });
+      }
+    }
+  });
 
   useLayoutEffect(() => {
     function start() {
-      let isMoving = false;
+      const lastKnownMovement = { x: 0, y: 0 };
+      const scaleFactor = 250;
 
       const moveCube = ({ movementX, movementY }) => {
-        if (!isMoving) return
-        mesh.current.rotation.x += movementY / 250;
-        mesh.current.rotation.y += movementX / 250;
+        mesh.current.rotation.x += movementY / scaleFactor;
+        mesh.current.rotation.y += movementX / scaleFactor;
+        lastKnownMovement.x = movementX;
+        lastKnownMovement.y = movementY;
       };
 
       window.addEventListener("mousedown", e => {
-        const { movementX, movementY } = e;
-        isMoving = true;
         window.addEventListener("mousemove", moveCube);
         window.addEventListener("mouseup", () => {
-
-
-          window.removeEventListener("mousemove", moveCube)
-        })
-        console.log(movementX, movementY);
+          setState({
+            x: lastKnownMovement.x / scaleFactor,
+            y: lastKnownMovement.y / scaleFactor,
+            isMoving: true
+          });
+          window.removeEventListener("mousemove", moveCube);
+        });
       });
     }
 
-    start()
+    start();
   }, []);
   return (
     <mesh {...props} ref={mesh} scale={[1, 1, 1]}>
       <boxGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial attach="material" map={texture} metalness={0.5} />
+      <meshStandardMaterial attach="material" map={texture} />
     </mesh>
   );
 }
