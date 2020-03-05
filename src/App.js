c4cf9d0e0988f82e497d24cfb83b07a558893876
Jonathan Ingall <jonny@mondago.com>
2020-03-05 13:04:22 +0000

    Add zoom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React, { Suspense } from "react";
-import { Canvas } from "react-three-fiber";
+import React, { Suspense, useState, useEffect, useRef } from "react";
+import { Canvas, useFrame, useThree } from "react-three-fiber";
 import Box from "./Box";
 
+function Camera(props) {
+  const ref = useRef();
+  const { setDefaultCamera } = useThree();
+  // Make the camera known to the system
+  useEffect(() => setDefaultCamera(ref.current), [setDefaultCamera]);
+  // Update it every frame
+  useFrame(() => ref.current.updateMatrixWorld());
+  return <perspectiveCamera ref={ref} {...props} />;
+}
+
 function App() {
+  const [scroll, setScroll] = useState(10);
+  const zoom = e => {
+    e.persist();
+    setScroll(prev => prev + e.deltaY / 150);
+  };
   return (
     <>
       <h1 style={{ textAlign: "center" }}>CultureGo is not your friend</h1>
-      <Canvas style={{ height: "90vh", width: "100vw" }}>
+      <Canvas style={{ height: "90vh", width: "100vw" }} onWheel={zoom}>
+        <Camera position={[0, 0, scroll]} />
         <ambientLight />
         <pointLight position={[16, 7, 5]} />
-        <perspectiveCamera fov={75} aspect={2} near={0.1} far={5} />
+
         <Suspense fallback="loading...">
           <Box position={[0, 0, 0]} />
-          <Box position={[1, 0, 0]} />
-          <Box position={[0, 1, 0]} />
+          <Box position={[1.5, 0, 0]} />
+          <Box position={[0, 1.5, 0]} />
         </Suspense>
       </Canvas>
     </>
